abbab3b592e4fb49da39fe21adfc509ebe31a5b5
piratesecurity <bharath.hussy@gmail.com>
2016-08-28 12:28:04 +0530

    main.py crawls forbes top 500 websites (reads from file)  robots.txt file from all websites

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import urllib2
 import threading
 import cookielib
 
+all_websites=[]
+
 
 def user_agent_handler(proxy_url):
 	cj = cookielib.FileCookieJar("cookies")
@@ -22,10 +24,34 @@ def user_agent_handler(proxy_url):
 	user_agent.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux i686 on x86_64; rv:23.0) Gecko/20100101 Firefox/23.0')]
 	return user_agent
 	
+def read_website_urls(file):
+	fd=open(file,"r")
+	contents=fd.read().split("\n")
+	for line in contents:
+		line_split=line.split("	")
+		try:
+			all_websites.append(line_split[2])
+		except:
+			pass
+	
+	fd.close()
+	#print all_websites	
+
 
 def crawl_website(site_url,user_agent):
-	content=urllib2.urlopen(site_url).read()
-	print content
+	try:
+		content=urllib2.urlopen(site_url).read()
+		write_file="data/"+site_url.split("/")[2]+".txt"
+		print write_file
+		print site_url
+		wfd=open(write_file,"w")
+		wfd.write(content)
+		wfd.close()
+		#print content
+	except:
+		print "-----------Site Failed-------------\n"
+		print site_url
+		print "+++++++++++End+++++++++++++++++++++\n"
 
 
 
@@ -33,10 +59,12 @@ def crawl_website(site_url,user_agent):
 
 
 def main():
-	print "Working"
-	user_agent=user_agent_handler("http://172.30.0.10:3128")
+	#print "Working"
+	read_website_urls("top_500.txt")
+	user_agent=user_agent_handler("http://172.30.0.22:3128")
 	urllib2.install_opener(user_agent)
-	crawl_website("https://www.google.co.in/robots.txt",user_agent)
+	for site in all_websites:
+		crawl_website("https://"+site+"/robots.txt",user_agent)
 
 
 
